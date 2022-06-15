f74e4b12e9c6125031b959128727fa049d1bd6d0
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2022-06-15 08:56:24 +0300

    chore: story sorting

--- storybook-react/src/components/Input/Input.stories.tsx
+++ storybook-react/src/components/Input/Input.stories.tsx
@@ -8,3 +8,5 @@ export default {
 export const Small = () => <Input size="small" placeholder="small size" />;
 export const Medium = () => <Input size="medium" placeholder="medium" />;
 export const Large = () => <Input size="large" placeholder="large size" />;
+
+Small.name = "Small input";
