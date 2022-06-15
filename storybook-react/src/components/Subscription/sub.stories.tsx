a61e818481b33ac81d305be861a3280f3ed2ff03
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2022-06-15 09:40:01 +0300

    chore: args

---
+++ storybook-react/src/components/Subscription/sub.stories.tsx
@@ -0,0 +1,13 @@
+import { Primary } from "../Button/Button.stories";
+import { Large } from "../Input/Input.stories";
+
+export default {
+  title: "formSubscription",
+};
+
+export const PrimarySubscription = () => (
+  <>
+    <Large />
+    <Primary />
+  </>
+);
